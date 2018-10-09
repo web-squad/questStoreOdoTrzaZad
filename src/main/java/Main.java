d3a248a0094a287e17508c8ce7cfbaf66501bf9c
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-10-09 15:50:31 +0200

    Established file structure.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,26 @@
+import controller.Logger;
+import view.View;
+
 public class Main {
-    public static void main(String[] args){}
+    public static void main(String[] args) {
+
+        Logger logger = new Logger();
+        View view = new View();
+        boolean isRunning = true;
+        int option;
+
+        while (isRunning) {
+            view.printMenu("Exit program", "Log in");
+            option = view.getInputInt(0,1);
+
+            switch (option) {
+                case 0:
+                    isRunning = false;
+                    break;
+                case 1:
+                    logger.logIn();
+                    break;
+            }
+        }
+    }
 }
--- src/main/java/controller/Logger.java
+++ src/main/java/controller/Logger.java
@@ -1,4 +1,36 @@
 package controller;
 
+import view.View;
+
+import java.sql.Connection;
+
 public class Logger {
+    private View view;
+    private String dbPass;
+    private String dbUser;
+    private Connection connection;
+
+    public Logger() {
+        view = new View();
+        dbPass = view.getInputString("DB Pass?");
+        dbUser = view.getInputString("DB User?");
+        connection = new Connector().connect(dbUser, dbPass);
+    }
+
+    public void logIn(){
+        userController = null;
+        String email = view.getInputString("Enter email: ");
+        setControllerIfEmailOnStudentsList(email);
+        setControllerIfEmailOnMentorsList(email);
+        setControllerIfEmailOnEmployeesList(email);
+        setControllerIfEmailOnManagersList(email);
+
+        if (!userInDatabase && userController == null) {
+            view.print("User with that email dose not exist");
+        }
+        else if (userController != null) {
+            userController.welcomeUser();
+            userController.startUserSession();
+        }
+    }
 }
