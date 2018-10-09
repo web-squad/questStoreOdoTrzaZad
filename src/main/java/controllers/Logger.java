9ecf9b22d8a6c2abd74cbfc094d9ed6975436632
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-10-09 16:30:30 +0200

    Added logger functionality.

--- src/main/java/controllers/Logger.java
+++ src/main/java/controllers/Logger.java
@@ -1,30 +1,27 @@
-package controller;
+package controllers;
 
-import view.View;
-
+import controllers.dao.LoginAccesDAO;
+import views.View;
 import java.sql.Connection;
+import java.util.Map;
 
 public class Logger {
     private View view;
     private String dbPass;
     private String dbUser;
     private Connection connection;
+    private boolean userInDatabase;
 
     public Logger() {
         view = new View();
         dbPass = view.getInputString("DB Pass?");
         dbUser = view.getInputString("DB User?");
         connection = new Connector().connect(dbUser, dbPass);
+        userInDatabase = false;
     }
 
     public void logIn(){
-        userController = null;
-        String email = view.getInputString("Enter email: ");
-        setControllerIfEmailOnStudentsList(email);
-        setControllerIfEmailOnMentorsList(email);
-        setControllerIfEmailOnEmployeesList(email);
-        setControllerIfEmailOnManagersList(email);
-
+        UserController userController = logUser();
         if (!userInDatabase && userController == null) {
             view.print("User with that email dose not exist");
         }
@@ -32,5 +29,30 @@ public class Logger {
             userController.welcomeUser();
             userController.startUserSession();
         }
+    }
+
+    private UserController logUser(){
+        String email = view.getInputString("Enter email: ");
+        String password = view.getInputString("Enter Password");
+        Map loginData = new LoginAccesDAO(connection).getLoginData;
+        int accesLevel = loginData.get(email).get(1);
+        if(loginData.get(email).get(0).equals(password) && accesLevel > 0) {
+            userInDatabase = true;
+            return createUserController(accesLevel, email);
+        }
+        return null;
+    }
+
+    private UserController createUserController(int accesLevel, String email){
+        if (accesLevel == 1){
+            return new CodecoolerController(email);
+        }
+        else if (accesLevel == 2){
+            return new MentorController(email);
+        }
+        else if (accesLevel == 3){
+            return new CreepyGuyController(email);
+        }
+        return null;
     }
 }
