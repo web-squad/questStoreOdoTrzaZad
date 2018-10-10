8d5e398656c0d93c47d7bfdc9c5f3b904a36cff9
kodoj <kodoj1@o2.pl>
2018-10-10 21:38:15 +0200

    Artifact and Quest models implemented

--- src/main/java/models/Artifact.java
+++ src/main/java/models/Artifact.java
@@ -1,4 +1,50 @@
 package models;
 
 public class Artifact extends Model{
+
+    private int id;
+    private String name;
+    private String description;
+    private int price;
+
+    public Artifact(int id, String name, String description, int price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Artifact(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/main/java/models/Quest.java
+++ src/main/java/models/Quest.java
@@ -1,4 +1,52 @@
 package models;
 
 public class Quest extends Model{
+
+    private int id;
+    private String name;
+    private String description;
+    private int reward;
+
+    public Quest(int id, String name, String description, int reward) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.reward = reward;
+    }
+
+    public Quest(String name, String description, int reward) {
+        this.name = name;
+        this.description = description;
+        this.reward = reward;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public void setReward(int reward) {
+        this.reward = reward;
+    }
+
+
 }
