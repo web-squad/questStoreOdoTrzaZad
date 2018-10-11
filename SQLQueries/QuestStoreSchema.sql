f4bf9d618cfe9bec579557ae2e60f7d9fef09aa5
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-10-11 11:48:53 +0200

    Added method signatures DAO interface

--- SQLQueries/QuestStoreSchema.sql
+++ SQLQueries/QuestStoreSchema.sql
@@ -1,4 +1,4 @@
---
+﻿--
 -- PostgreSQL database dump
 --
 
@@ -139,7 +139,7 @@ CREATE SEQUENCE public.codecooler_id_seq
     NO MAXVALUE
     CACHE 1;
 
-ALTER SEQUENCE public.codecooler_id_seq OWNED BY public.codecoolers.codecoolers_id;
+ALTER SEQUENCE public.codecooler_id_seq OWNED BY public.codecoolers.codecooler_id;
 
 
 ALTER TABLE public.codecoolers OWNER TO queststore;
