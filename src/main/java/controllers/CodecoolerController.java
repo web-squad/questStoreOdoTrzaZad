719025160863d0d147c9acc0d88272100dc9c6bf
Karol Trzaska <karolantonitrzaska@gmail.com>
2018-10-11 09:09:41 +0200

    CodecoolerController extends userController

--- src/main/java/controllers/CodecoolerController.java
+++ src/main/java/controllers/CodecoolerController.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class CodecoolerController {
+public class CodecoolerController extends UserController {
     private CodecoolerModel codecoolerModel;
     private CodecoolerDAO codecoolerControllerDAO;
     private View view;
@@ -23,7 +23,7 @@ public class CodecoolerController {
     }
 
 
-    public void run() {
+    public void startUserSession() {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String answer = "";
         boolean isRunning = true;
