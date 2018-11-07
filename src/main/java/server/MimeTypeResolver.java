55504d0b69ab9b953a1b904f6f1d769aa6309085
kodoj <kodoj1@o2.pl>
2018-11-07 13:32:56 +0100

    mentor menu updated to redirect to mentor pages

--- src/main/java/server/MimeTypeResolver.java
+++ src/main/java/server/MimeTypeResolver.java
@@ -1,7 +1,14 @@
 package server;
 
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,6 +28,29 @@ public class MimeTypeResolver {
         Matcher matcher = pattern.matcher(file.getName());
         matcher.find();
         return matcher.group(1);
+    }
+
+    public static class MentorWelcomePage implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+
+
+            // get a template file
+            JtwigTemplate template = JtwigTemplate.classpathTemplate("HTML/mentorPages/mentorWelcomePage.twig");
+
+            // create a model that will be passed to a template
+            JtwigModel model = JtwigModel.newModel();
+
+            // render a template to a string
+            String response = template.render(model);
+
+            // send the results to a the client
+            httpExchange.sendResponseHeaders(200, response.length());
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+
+        }
     }
 }
 
