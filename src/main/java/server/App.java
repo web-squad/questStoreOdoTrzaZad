489f8aff2a2bc171eb1639d1f9223cee78e7cc17
kodoj <kodoj1@o2.pl>
2018-11-07 11:36:08 +0100

    java class for every page and all page added in App.java

--- src/main/java/server/App.java
+++ src/main/java/server/App.java
@@ -1,6 +1,9 @@
 package server;
 
 import com.sun.net.httpserver.HttpServer;
+import server.codecoolerJavaPages.*;
+import server.adminJavaPages.*;
+import server.mentorJavaPages.*;
 
 import java.net.InetSocketAddress;
 
@@ -11,9 +14,43 @@ public class App {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
         // set routes
-        server.createContext("/CodecoolerIndex", new CodecoolerIndex());
+        server.createContext("codecoolerJavaPages/CodecoolerIndex", new CodecoolerIndex());
+        server.createContext("codecoolerJavaPages/CodecoolerMain", new CodecoolerMain());
+        server.createContext("codecoolerJavaPages/CreateTeam", new CreateTeam());
+        server.createContext("codecoolerJavaPages/EditUserTeam", new EditUserTeam());
+        server.createContext("codecoolerJavaPages/Store", new Store());
+        server.createContext("codecoolerJavaPages/UserArtifacts", new UserArtifacts());
+
+        server.createContext("mentorJavaPages/MentorAddArtifact", new MentorAddArtifact());
+        server.createContext("mentorJavaPages/MentorAddQuest", new MentorAddQuest());
+        server.createContext("mentorJavaPages/MentorAddStudent", new MentorAddStudent());
+        server.createContext("mentorJavaPages/MentorCheckWallet", new MentorCheckWallet());
+        server.createContext("mentorJavaPages/MentorEditArtifact", new MentorEditArtifact());
+        server.createContext("mentorJavaPages/MentorEditQuest", new MentorEditQuest());
+        server.createContext("mentorJavaPages/MentorEditStudent", new MentorEditStudent());
+        server.createContext("mentorJavaPages/MentorIndexPage", new MentorIndexPage());
+        server.createContext("mentorJavaPages/MentorMarkItemAsUsed", new MentorMarkItemAsUsed());
+        server.createContext("mentorJavaPages/MentorMarkQuestAsCompleted", new MentorMarkQuestAsCompleted());
+        server.createContext("mentorJavaPages/MentorQuests", new MentorQuests());
+        server.createContext("mentorJavaPages/MentorRemoveStudent", new MentorRemoveStudent());
+        server.createContext("mentorJavaPages/MentorShop", new MentorShop());
+        server.createContext("mentorJavaPages/MentorWelcomePage", new MentorWelcomePage());
+
+        server.createContext("adminJavaPages/AdminMain", new AdminMain());
+        server.createContext("adminJavaPages/ClassAdder", new ClassAdder());
+        server.createContext("adminJavaPages/ClassDeleter", new ClassDeleter());
+        server.createContext("adminJavaPages/ClassEditor", new ClassEditor());
+        server.createContext("adminJavaPages/ExpLVLAdder", new ExpLVLAdder());
+        server.createContext("adminJavaPages/ExpLVLDeleter", new ExpLVLDeleter());
+        server.createContext("adminJavaPages/ExpLVLEditor", new ExpLVLEditor());
+        server.createContext("adminJavaPages/GreetAdmin", new GreetAdmin());
+        server.createContext("adminJavaPages/MentorAdder", new MentorAdder());
+        server.createContext("adminJavaPages/MentorDeleter", new MentorDeleter());
+        server.createContext("adminJavaPages/MentorEditor", new MentorEditor());
         server.createContext("/static", new Static());
         server.setExecutor(null); // creates a default executor
+
+
 
         // start listening
         server.start();
---
+++ src/main/java/server/mentorJavaPages/MentorAddQuest.java
@@ -0,0 +1,33 @@
+package server.mentorJavaPages;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
+public class MentorAddQuest implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+
+        // get a template file
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("HTML/mentorPages/mentorAddQuest.twig");
+
+        // create a model that will be passed to a template
+        JtwigModel model = JtwigModel.newModel();
+
+        // render a template to a string
+        String response = template.render(model);
+
+        // send the results to a the client
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+
+    }
+}
