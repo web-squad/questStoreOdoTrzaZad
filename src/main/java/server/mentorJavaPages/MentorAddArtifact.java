3b5943b2c8f970dea56dac152cd9abf19cad698f
kodoj <kodoj1@o2.pl>
2018-11-08 14:22:25 +0100

    merge commit

--- src/main/java/server/mentorJavaPages/MentorAddArtifact.java
+++ src/main/java/server/mentorJavaPages/MentorAddArtifact.java
@@ -3,23 +3,46 @@ package server.mentorJavaPages;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
-
+import java.io.*;
+import java.net.HttpCookie;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import controllers.dao.LoginAccesDAO;
+import controllers.dao.MentorDAO;
+import models.Artifact;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
+import server.helpers.CookieHelper;
 
 public class MentorAddArtifact implements HttpHandler {
+    private static final String SESSION_COOKIE_NAME = "sessionId";
+    private MentorDAO mentorDAO;
+    private LoginAccesDAO loginAccesDAO;
+    private CookieHelper cookieHelper;
+
+    public MentorAddArtifact(MentorDAO mentorDAO, LoginAccesDAO loginAccesDAO) {
+        this.mentorDAO = mentorDAO;
+        this.cookieHelper = new CookieHelper();
+        this.loginAccesDAO = loginAccesDAO;
+    }
+
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-
+        String method = httpExchange.getRequestMethod();
+        Optional<HttpCookie> httpCookie = getSessionIdCookie(httpExchange);
+        String sessionId = httpCookie.get().getValue().replace("\"", "");
+        System.out.println(sessionId);
 
         // get a template file
         JtwigTemplate template = JtwigTemplate.classpathTemplate("HTML/mentorPages/mentorAddArtifact.twig");
 
-        // create a model that will be passed to a template
         JtwigModel model = JtwigModel.newModel();
-
         // render a template to a string
         String response = template.render(model);
 
@@ -29,5 +52,39 @@ public class MentorAddArtifact implements HttpHandler {
         os.write(response.getBytes());
         os.close();
 
+
+        if (method.equals("POST")) {
+            Map inputs = getData(httpExchange);
+            String providedArtifactName = inputs.get("name").toString();
+            String providedArtifactDescription = inputs.get("description").toString();
+            String providedArtifactPrice = inputs.get("price").toString();
+            Artifact newArtifact = new Artifact(providedArtifactName, providedArtifactDescription, Integer.parseInt(providedArtifactPrice));
+            mentorDAO.addArtifactToStore(newArtifact);
+        }
+    }
+
+    private Optional<HttpCookie> getSessionIdCookie(HttpExchange httpExchange){
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+        List<HttpCookie> cookies = cookieHelper.parseCookies(cookieStr);
+        return cookieHelper.findCookieByName(SESSION_COOKIE_NAME, cookies);
+    }
+
+    private Map<String, String> getData(HttpExchange httpExchange) throws IOException{
+        InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), StandardCharsets.UTF_8);
+        BufferedReader br = new BufferedReader(isr);
+        String formData = br.readLine();
+        return parseFormData(formData);
+    }
+
+    private static Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
+        Map<String, String> map = new HashMap<>();
+        String[] pairs = formData.split("&");
+        for(String pair : pairs){
+            String[] keyValue = pair.split("=");
+            // We have to decode the value because it's urlencoded. see: https://en.wikipedia.org/wiki/POST_(HTTP)#Use_for_submitting_web_forms
+            String value = URLDecoder.decode(keyValue[1], "UTF-8");
+            map.put(keyValue[0], value);
+        }
+        return map;
     }
 }
