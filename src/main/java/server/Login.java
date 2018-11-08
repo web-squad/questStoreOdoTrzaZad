b9969df16a8880715d5d56d7d0fe441f7fa3862c
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-11-08 11:37:02 +0100

    Moved getSessionID method to cookieHelper class

--- src/main/java/server/Login.java
+++ src/main/java/server/Login.java
@@ -22,7 +22,6 @@ import java.util.Optional;
 
 public class Login implements HttpHandler {
     String activeSession;
-    private static final String SESSION_COOKIE_NAME = "sessionId";
     int counter = 0;
     CookieHelper cookieHelper = new CookieHelper();
     LoginAccesDAO loginAccesDAO;
@@ -39,7 +38,7 @@ public class Login implements HttpHandler {
         Optional<HttpCookie> cookie;
 
         if(method.equals("GET")) {
-            cookie = getSessionIdCookie(httpExchange);
+            cookie = cookieHelper.getSessionIdCookie(httpExchange);
             if (cookie.isPresent()) {
                 loginAccesDAO.deleteSessionID(cookie.get().getValue());
             }
@@ -62,7 +61,7 @@ public class Login implements HttpHandler {
                 int accessLevel = loginData.get(0);
                 int id = loginData.get(1);
                 String sessionId = String.valueOf(hash(providedMail + providedPassword + LocalDateTime.now().toString()));
-                cookie = Optional.of(new HttpCookie(SESSION_COOKIE_NAME, sessionId));
+                cookie = Optional.of(new HttpCookie(cookieHelper.getSessionCookieName(), sessionId));
                 activeSession = sessionId;
                 loginAccesDAO.saveSessionId(sessionId, providedMail);
                 httpExchange.getResponseHeaders().add("Set-Cookie", cookie.get().toString());
@@ -114,11 +113,6 @@ public class Login implements HttpHandler {
         return map;
     }
 
-    private Optional<HttpCookie> getSessionIdCookie(HttpExchange httpExchange){
-        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
-        List<HttpCookie> cookies = cookieHelper.parseCookies(cookieStr);
-        return cookieHelper.findCookieByName(SESSION_COOKIE_NAME, cookies);
-    }
 
     private Map<String, String> getData(HttpExchange httpExchange) throws IOException{
         InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), StandardCharsets.UTF_8);
