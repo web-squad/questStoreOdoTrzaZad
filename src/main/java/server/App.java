f5f9a210bdb79f5a33a78dc4b5603382e7fa7618
Adelaiten <karoltrzaska19@gmail.com>
2018-11-09 13:13:36 +0100

    css improved

--- src/main/java/server/App.java
+++ src/main/java/server/App.java
@@ -15,8 +15,8 @@ import java.sql.Connection;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String dbPass = "quest";
-        String dbUser = "queststore";
+        String dbPass = "4313284";
+        String dbUser = "karol";
         Connection connection = new Connector().connect(dbUser, dbPass);
         // create a server on port 8000
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
