e6854dd5586a5be844ee99e9895d42bfe26b313b
Andrzej Rzeszut <andrzejrz556@gmail.com>
2019-03-26 09:53:20 +0100

    Create test folder with first test empty class

---
+++ src/main/java/tests/CodecoolerDAOTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CodecoolerDAOTest {
+
+    @Test
+    void getNickName() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void getSecondName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+}
